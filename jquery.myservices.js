80d0b9dc1f2d65fecb4ba95b99c8707e885ab9fa
Justin Greco <justin.greco@raleighnc.gov>
2019-03-28 11:17:21 -0400

    Update jquery.myservices.js

--- jquery.myservices.js
+++ jquery.myservices.js
@@ -53,7 +53,7 @@
 		title: "Hospitality District",
 		url: "https://services.arcgis.com/v400IkDOw1ad7Yad/arcgis/rest/services/Hospitality_District_Form/FeatureServer/1/query",
 		texts: [{
-		   title: "<a href='https://www.raleighnc.gov/home/content/CityMgrAdmin/Articles/GlenwoodSouthHospitalityDistrict.html'>Inside Hospitality District</a>:",
+		   title: "<a href='https://www.raleighnc.gov/home/content/CityMgrAdmin/Articles/GlenwoodSouthHospitalityDistrict.html'>Inside Hospitality District?</a>:",
 		   labels: "Yes"
 		}]
 	       }
